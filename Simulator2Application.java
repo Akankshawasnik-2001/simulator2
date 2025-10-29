d8d5ca4a033af8e79df9d2ca7a67016d7a1ea950
Akanksha wasnik <akankshawasnik725@gmail.com>
2025-10-29 15:53:48 +0530

    Add files via upload

---
+++ Simulator2Application.java
@@ -0,0 +1,13 @@
+package com.iot.dashboard.simulator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simulator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simulator2Application.class, args);
+	}
+
+}
---
+++ Simulator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.dashboard.simulator2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simulator2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
