66fc4799e61e482b6ed87dbaa822155c20e1b9bf
Akanksha wasnik <akankshawasnik725@gmail.com>
2025-10-30 16:09:01 +0530

    Add files via upload

---
+++ DeviceDataRepository.java
@@ -0,0 +1,8 @@
+package com.iot.dashboard.simulator2.repository;
+
+import com.iot.dashboard.simulator2.Model.DeviceData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeviceDataRepository extends JpaRepository <DeviceData,Long>{
+
+}
